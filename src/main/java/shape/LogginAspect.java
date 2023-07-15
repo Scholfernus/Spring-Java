b7ee42af83c97d4b0fde0705b0be105f42b5c726
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-15 14:04:34 +0200

    Poprawki

---
+++ src/main/java/shape/LogginAspect.java
@@ -0,0 +1,22 @@
+package shape;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Around;
+
+@Aspect
+@Component
+public class LogginAspect {
+
+    @Around("execution(* shape.Square.calculateArea())")
+    public Object logSquareArea(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("Obliczanie pola figury...");
+
+        // Wykonanie metody, do której zostało zastosowane @Around
+        Object result = joinPoint.proceed();
+
+        System.out.println("Obliczenie pola figury zakończone.");
+
+        return result;
+    }
+}
