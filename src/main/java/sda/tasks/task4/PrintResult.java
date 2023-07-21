8738e22976643154f9da00a6df3fff77aa4d520b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-21 19:11:47 +0200

    Usunąłem klasę testową. Zmiana kodu PrintResult - dodałem kod prowadzącego Arka.

--- src/main/java/sda/tasks/task4/PrintResult.java
+++ src/main/java/sda/tasks/task4/PrintResult.java
@@ -1,20 +1,32 @@
 package sda.tasks.task4;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 @SpringBootApplication
-public class PrintResult {
+public class PrintResult implements CommandLineRunner {
     @Autowired
     private DummyLogger dummyLogger;
     @Autowired
     private ListUtil listUtil;
     @Autowired
     private StringUtil stringUtil;
+    private final ApplicationContext applicationContext;
+
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(PrintResult.class, args);
@@ -25,9 +37,20 @@ public class PrintResult {
     public void print() {
         dummyLogger.sayHello();
         int sum = listUtil.sumElements(List.of(1, 2, 3));
-        String sentence = stringUtil.formSentence(List.of("Goodbye", "cruel", "world"));
+        String sentence = stringUtil.formSentence(List.of("Goodbye", "cruel", "world."));
 
         System.out.println("Suma wszystkich elementów, to " + sum);
         System.out.println(sentence);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Set<String> beanNames =
+                Arrays.stream(applicationContext.getBeanDefinitionNames())
+                        .filter(b -> !b.contains("."))
+                        .collect(Collectors.toSet());
+        final Set<String> correctNames = Set.of("dummyLogger", "listUtility",
+                "stringUtility");
+        log.info("Zadanie rozwiązanie poprawne? {}", beanNames.containsAll(correctNames));
     }
 }
--- src/main/java/sda/tasks/task4/Task4ApplicationTests.java
+++ src/main/java/sda/tasks/task4/Task4ApplicationTests.java
@@ -1,28 +1,28 @@
-package sda.tasks.task4;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.Test;
-
-import java.util.List;
-
-@SpringBootTest
-public class Task4ApplicationTests {
-    @Autowired
-    @Qualifier
-    private DummyLogger dummyLogger;
+//package sda.tasks.task4;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.Test;
+//
+//import java.util.List;
+//
+//@SpringBootTest
+//public class Task4ApplicationTests {
 //    @Autowired
-//    private ListUtil listUtil;
-//    @Autowired
-//    private StringUtil stringUtil;
-    @Test
-    public void testBeans(){
-        DummyLogger dummyLogger = new DummyLogger();
-        ListUtil listUtil = new ListUtil();
-        StringUtil stringUtil = new StringUtil();
-        dummyLogger.sayHello();
-        System.out.println("Suma wszystkich elementów, to " + listUtil.sumElements(List.of(1,2,3)));
-        System.out.println(stringUtil.formSentence(List.of("Goodbye","cruel", "world")));
-    }
-}
+//    @Qualifier
+//    private DummyLogger dummyLogger;
+////    @Autowired
+////    private ListUtil listUtil;
+////    @Autowired
+////    private StringUtil stringUtil;
+//    @Test
+//    public void testBeans(){
+//        DummyLogger dummyLogger = new DummyLogger();
+//        ListUtil listUtil = new ListUtil();
+//        StringUtil stringUtil = new StringUtil();
+//        dummyLogger.sayHello();
+//        System.out.println("Suma wszystkich elementów, to " + listUtil.sumElements(List.of(1,2,3)));
+//        System.out.println(stringUtil.formSentence(List.of("Goodbye","cruel", "world")));
+//    }
+//}
