7755bb7b807a67a8d482ea1d0d3a4916e47b796a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-23 09:58:41 +0200

    Przeniesienie do pakietu

--- src/main/java/sda/tasks/task4/config/UtilConfiguration.java
+++ src/main/java/sda/tasks/task4/config/UtilConfiguration.java
@@ -1,18 +1,21 @@
-package sda.tasks.task4;
+package sda.tasks.task4.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import sda.tasks.task4.DummyLogger;
+import sda.tasks.task4.ListUtil;
+import sda.tasks.task4.StringUtil;
 
 @Configuration
 public class UtilConfiguration {
     @Bean
     @Primary
-    public DummyLogger customDummyLogger(){
+    public DummyLogger dummyLogger(){
         return new DummyLogger();
     }
     @Bean
-    public ListUtil listUtil(){
+    public ListUtil listUtility(){
         return new ListUtil();
     }
     @Bean(name="stringUtility")
